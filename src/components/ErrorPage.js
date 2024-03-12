f326d9eba90496f7ebf47eb01b5836465111b15e
Karey Powell <kareypowell@gmail.com>
2024-03-12 18:51:54 -0500

    Add foundation for entire app layout

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,22 @@
+import { Link, useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h1 className="mt-3">Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+            <em>{error.statusText || error.message}</em>
+          </p>
+          <p>
+            <Link to="/">Return to the Home page</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
